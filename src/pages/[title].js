835559c638af41ac1dd147eb859cc44a3e6bd1f3
unholydisaster <eli.musa.173@gmail.com>
2023-05-02 01:42:59 +0300

    changes

---
+++ src/pages/[title].js
@@ -0,0 +1,83 @@
+import React from "react";
+import ShareButton from "../components/ShareLinks.js"
+import { ArticleContainer,Article } from "@/styles/articles/articlepage";
+import CodeBlock from "@/styles/codecontainers/highlighter";
+import remarkGfm from "remark-gfm";
+import axios from "axios"
+
+
+export default function NotesByTitle({ note,noteTitle,noteImage}) {
+  
+  return(
+    <> 
+    <ShareButton noteTitle={noteTitle} noteImage={noteImage}/>
+    <ArticleContainer>
+    <Article 
+      skipHtml={false}
+      components={CodeBlock}
+      remarkPlugins={[remarkGfm]}>
+    {note}
+    </Article>
+    </ArticleContainer>
+    </>
+
+  )
+}
+
+
+export async function getStaticPaths() {
+  const BASE_URL = process.env.BASE_URL;
+  const {data} = await axios.get(`${BASE_URL}api/notes/`)
+
+  const paths = data.map((article) => ({
+    params: {
+      title: article.title,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { title } = params;
+  const BASE_URL = process.env.BASE_URL;
+  try {
+    
+    const {data} = await axios.get(`${BASE_URL}api/notes/${title}`)
+
+    /*
+    const res = await fetch(`${BASE_URL}api/notes`)
+    const notesdata = await res.json()
+    const items = [...notesdata];
+
+    // Shuffle the order of the items array using the Fisher-Yates shuffle algorithm
+  
+    for (let i = items.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [items[i], items[j]] = [items[j], items[i]];
+    }
+    */
+
+    return {
+      props: {
+        note: data.note,
+        noteTitle:data.title,
+        noteImage:data.imageUrl,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching note:", error);
+    return {
+      props: {
+        note: "",
+        noteTitle:"",
+        noteImage:"",
+      },
+    };
+  }
+}
+  
+  
